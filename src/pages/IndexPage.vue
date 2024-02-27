636a18a00d89066913f54c785cedd287bdd1d026
m11011111 <mk@elm-tech.ru>
2024-02-27 15:57:56 +0300

    test tg-web-view

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,19 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1><span class="text-light-green-8">FLEX</span>COIN</h1>
+  <q-page class="row column">
+    <h4><span class="text-light-green-8">FLEX</span>COIN</h4>
+    <q-form>
+      <q-input
+        v-model="userName"
+        outlined
+      />
+      <Alert message="Hello!" />
+    </q-form>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import {Alert} from 'vue-tg';
 
+const userName = ref('')
 </script>
