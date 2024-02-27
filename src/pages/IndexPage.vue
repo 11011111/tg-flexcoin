a9d0bddc992dc217118e926d84aaa2b3b342c259
m11011111 <mk@elm-tech.ru>
2024-02-27 14:54:04 +0300

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <h1><span class="text-light-green-8">FLEX</span>COIN</h1>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
