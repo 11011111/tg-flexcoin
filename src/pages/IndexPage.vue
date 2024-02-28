28a21ef349475ba8f0e68daa9f11262e44fe54d2
m11011111 <mk@elm-tech.ru>
2024-02-28 16:57:47 +0300

    main settings

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,6 +15,8 @@ q-page.row.column.q-pa-md
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { authRequest } from 'src/common/requests'
+import { api } from 'boot/axios'
+import { apiLinks } from 'src/common/routerLinks'
 
 const userName = ref('')
 const initDt = ref()
@@ -44,16 +46,16 @@ const sendMessage = () => {
   }
 
   initDt.value = tg.initData
+  // 'query_id=AAHfvhIKAAAAAN--EgqlonTI&user=%7B%22id%22%3A169000671%2C%22first_name%22%3A%22%D0%9C%D0%B0%D0%BA%D1%81%D0%B8%D0%BC%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22m11011111%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1709127503&hash=a0379d456320ddfe387507342c4aec3f7d25761904abd2d14cece35ffe6c3e74'
 
-  console.log(initDt.value)
-
-  // authRequest({ method: 'get', {initDt.value} )
-  //   .then((r) => {
-  //     console.log(r.data)
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
+  api
+    .get(`${apiLinks.AUTH.list}?${initDt.value}`)
+    .then((r) => {
+      console.log(r.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 
   // exchange
   //   .getAuth(initDt.value)
