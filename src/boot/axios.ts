39144b5cfd7ed9b13ba50d110e8145f094410cda
m11011111 <mk@elm-tech.ru>
2024-02-29 16:58:55 +0300

    main bugfix yes token

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -18,13 +18,13 @@ const api = axios.create({
 })
 
 // Добавляем перехват запросов
-// api.interceptors.request.use(function (config: InternalAxiosRequestConfig) {
-//   const token = localStorage.getItem('access')
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-//   return config
-// })
+api.interceptors.request.use(function (config: InternalAxiosRequestConfig) {
+  const token = localStorage.getItem('access')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 // Добавляем перехват ответов
 api.interceptors.response.use(
