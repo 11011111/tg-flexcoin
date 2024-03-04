5c19832c30cdb4b4fa096dd8e3a98dde11d4d017
m11011111 <mk@elm-tech.ru>
2024-03-04 10:42:55 +0300

    test log platform

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,8 +19,12 @@ const tg = window.Telegram.WebApp
 onMounted(() => {
   tg.ready()
 
-  // let data =
-  //   'query_id=AAHfvhIKAAAAAN--EgqlonTI&user=%7B%22id%22%3A169000671%2C%22first_name%22%3A%22%D0%9C%D0%B0%D0%BA%D1%81%D0%B8%D0%BC%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22m11011111%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1709127503&hash=a0379d456320ddfe387507342c4aec3f7d25761904abd2d14cece35ffe6c3e74'
-  login(tg.initData)
+  console.log(tg.platform)
+
+  // const accessToken = localStorage.getItem('access')
+
+  // if (accessToken) return
+
+  // login(tg.initData)
 })
 </script>
