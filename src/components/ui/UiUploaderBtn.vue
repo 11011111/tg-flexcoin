599fd69a03522b0b3257b19b64a6a88603829c99
m11011111 <mk@elm-tech.ru>
2024-03-24 13:51:16 +0300

    hotfix

--- src/components/ui/UiUploaderBtn.vue
+++ src/components/ui/UiUploaderBtn.vue
@@ -41,6 +41,7 @@ watch(fileUpload, (file) => {
         if (res.data.ok) router.push({ name: links.EXCHANGE.name })
       })
       .catch((err) => {
+        alert(err)
         console.log(err)
         dialogWindow.value = true
         fileUpload.value = null
